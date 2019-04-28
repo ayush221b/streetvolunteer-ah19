6dd4af86be66e2d7e998e6cea8f553b886b7e203
pratik037 <singhalpratik037@gmail.com>
2019-04-28 09:47:56 +0530

    Changes

--- lib/app/pages/drawer.dart
+++ lib/app/pages/drawer.dart
@@ -1,44 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:streetvolunteer_ah19/app/scoped_models/main.dart';
 
-Widget draw = Drawer(
-        child: Column(
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              currentAccountPicture: CircleAvatar(
-                backgroundColor: Colors.white,
-              ),
-              accountName: Text(
-                "Pratik Singhal",
-                style: TextStyle(fontSize: 20),
-              ),
+class DrawerMenu extends StatefulWidget {
+  final MainModel model;
+
+  const DrawerMenu({Key key, this.model}) : super(key: key);
+  @override
+  _DrawerMenuState createState() => _DrawerMenuState();
+}
+
+class _DrawerMenuState extends State<DrawerMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            accountEmail: Text(''),
+            currentAccountPicture: CircleAvatar(
+              backgroundColor: Colors.white,
             ),
-
-
-            ListTile(
-              leading: Icon(Icons.add),
-              title: Text("Create Campaign"),
+            accountName: Text(
+              "Pratik Singhal",
+              style: TextStyle(fontSize: 20),
             ),
-
-            ListTile(
-              leading: Icon(FontAwesomeIcons.peopleCarry),
-              title: Text("Browse Campaign"),
-            ),
-
-            ListTile(
-              leading: Icon(Icons.person),
-              title: Text("My Profile"),
-            ),
-
-            ListTile(
-              leading: Icon(FontAwesomeIcons.personBooth),
-              title: Text("My Campaigns"),
-            ),
-
-            ListTile(
-              leading: Icon(Icons.exit_to_app),
-              title: Text("Logout"),
-            ),
-          ],
-        ),
-      );
+          ),
+          ListTile(
+            leading: Icon(Icons.add),
+            title: Text("Create Campaign"),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.peopleCarry),
+            title: Text("Browse Campaign"),
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text("My Profile"),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.personBooth),
+            title: Text("My Campaigns"),
+          ),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text("Logout"),
+          ),
+        ],
+      ),
+    );
+  }
+}
